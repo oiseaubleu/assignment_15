6f9bc5610db7be319966df5ec2ea003114a3b93b
michiru <michiru.shigemitsu@gmail.com>
2024-06-17 13:23:09 +0900

    using asynchronous communication

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,28 @@
 class FavoritesController < ApplicationController
 
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    @fav = current_user.favorites.build(blog_id: params[:blog_id])
+    #binding.irb
+    respond_to do |format|
+      if @fav.save
+        flash.now[:notice] = 'お気に入りに登録しました'
+        format.js { render :favorite }
+      else
+        flash.now[:notice] = 'お気に入りの登録に失敗しました'
+        format.js { render :favorite }
+      end
+    end
+
   end
 
   def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    #current_user.favorites.find_by(id: params[:id]).destroy
+    @fav = current_user.favorites.find_by(id: params[:id])
+    @fav.destroy!
+    respond_to do |format|
+      flash.now[:notice] = 'お気に入りが削除されました'
+      format.js { render :favorite }
+    end
   end
 
 end
